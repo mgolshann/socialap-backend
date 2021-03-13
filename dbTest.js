0d9871212590eedcff252e9249cbba05334de97e
mgolshann <m_golshan85@yahoo.com>
2021-03-13 10:47:30 -0800

    working with some functions to deal with mongodb

--- dbTest.js
+++ dbTest.js
@@ -33,7 +33,6 @@ const newCourse = new CourseModel({
     price: 300000
 });
 
-
 // newCourse
 //     .save()
 //     .then(res => {
@@ -51,34 +50,41 @@ async function createCourse() {
 
 async function getCourseList() {
     // const courseList = await CourseModel.find();
+    const pageSize = 2;
+    const pageNumber = 2;
     const courseList = await CourseModel
-        // .find({
-        //     completed: false,
-        //     teacher: "ali golshan2"
-        // })
-        // .limit(3)
-        // .sort({ name: 1 }) /* 1 ASC -1 DESC */
-        // .select({ name: 1, tags: 1 });
-        // .find({
-        //     price: {
-        //         $gte: 100000,
-        //         $lte: 200000
-        //     }
-        // })
-        // .find({
-        //     price: {
-        //         $in : [100000, 70000, 50000]
-        //     }
-        // })
-        // .find()
-        // .or([{ completed: true }, { price: 100000}])
-        // .and([{ completed: true }, { price: 100000}])
-        // .sort({ name: 1 })
-        .find({ teacher: /^ali/ }) // لیست کسایی که ابتدای اسمشون محمد دار رو بر میگردونه
-        .find({ teacher: /golshan$/ }) // دوره های اسم آخرشون گلشنه
-        .find({ teacher: /.*golshan.*/ }) // اون وسط گلشن باشه قبل و بعدش هر کارکتری که اومد ایراد نداره
+        .find()
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+    // .find({
+    //     completed: false,
+    //     teacher: "ali golshan2"
+    // })
+    // .limit(3)
+    // .sort({ name: 1 }) /* 1 ASC -1 DESC */
+    // .select({ name: 1, tags: 1 });
+    // .find({
+    //     price: {
+    //         $gte: 100000,
+    //         $lte: 200000
+    //     }
+    // })
+    // .find({
+    //     price: {
+    //         $in : [100000, 70000, 50000]
+    //     }
+    // })
+    // .find()
+    // .or([{ completed: true }, { price: 100000}])
+    // .and([{ completed: true }, { price: 100000}])
+    // .sort({ name: 1 })
+    // .find({ teacher: /^ali/ }) // لیست کسایی که ابتدای اسمشون محمد دار رو بر میگردونه
+    // .find({ teacher: /golshan$/ }) // دوره های اسم آخرشون گلشنه
+    // .find({ teacher: /.*golshan.*/ }) // اون وسط گلشن باشه قبل و بعدش هر کارکتری که اومد ایراد نداره
+    // .count();
     console.log(courseList);
 }
+//getCourseList();
 
 // eq => equal to
 // ne => not eqyal to
@@ -89,5 +95,39 @@ async function getCourseList() {
 // in
 //nin
 
-getCourseList();
 
+// approach 1
+// async function updateCourseName(id, name) {
+//     const course = await CourseModel.findById(id);
+//     if (!course) return;
+//     course.name = name;
+//     console.log(await course.save());
+
+// }
+// updateCourseName("604cd07b794b514bbc595742", "mgbg");
+
+//approach 2
+async function updateCourseName(id, name) {
+    // const result = await CourseModel.update(
+    //     {
+    //         _id: id
+    //     },
+    //     {
+    //         $set: {
+    //             name: name
+    //         },
+    //     },
+    // );
+    // console.log(result)
+
+    const result = await CourseModel.findByIdAndUpdate(id, {
+            $set: {
+                name: name
+            },
+        },
+        {new: true} // وقتی که آپدیت کردی خروجی جدید رو نشون بده نه اینکه همون قبلی
+    );
+    console.log(result)
+}
+
+updateCourseName("604ccffd0ef6625664180434", "Mohammad Golshan 3");
